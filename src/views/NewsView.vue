3f10632d55420712619530e33848b93546de9da4
hk0305 <hyounkeun2009@gmail.com>
2021-06-10 23:28:50 +0900

    * Vuex 설치  $ npm i vuex --save
     * store 폴더에 index.js 생성 후 main.js vuex store 설정
     - 크롬 개발자 도구의 Vue dev 툴의 시계모양 클릭하면 state 확인 가능

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -17,13 +17,13 @@ export default {
   created() {
     // 객체를 맵핑
     var vm = this;
-    console.log('호출 전 :', this); // 전역 this
+    // console.log('호출 전 :', this); // 전역 this
     // axios는 promise 기반
     fetchNewsList()
       .then(response => {
         // 화살 함수 선언을 사용하면 vm = this 바인딩을 하지 않아도 this를 바로 사용할 수 있다.
         // then(function(response) { console.log(this);}) 이렇게 선언하면 this는 function 내부를 가르킨다.
-        console.log('호출 후 :', this);
+        // console.log('호출 후 :', this);
         vm.users = response.data;
       })
       .catch(err => console.error(err))
