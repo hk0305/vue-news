2fe5ffef6bdb49c2f45933d7d1b5de24acc429f6
hk0305 <hyounkeun2009@gmail.com>
2021-06-12 22:57:27 +0900

    * store API 연결 및 화면에 호출  - API => vuex store (actions -> mutations -> state) => 저장된 state를 NewsView에 전달.

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,31 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { fetchNewsList } from '../api/index.js';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+    // 저장된 state는 NewsView에서 호출
     state : {
         news: []
+    },
+    // context.commit의 mutaion으로 전달받은 값을 stats로 전달
+    mutations: {
+        SET_NEWS(state, news) {
+            state.news = news;
+        }
+    },
+    // NewsView에서의 API actions - context 연결 및 response data 전달
+    actions: {
+        FETCH_NEWS(context) {
+          fetchNewsList()
+            .then(response => {
+              console.log(response);
+              // mutations와 연결
+              context.commit('SET_NEWS', response.data);
+
+            })
+            .catch(err => console.error(err));
+        }
     }
 });
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,12 @@
 <!-- <vue>로 vue file 자동완성 -->
 <template>
   <div>
-      <div v-for="user in users" v-bind:key="user.id">{{ user.title }}</div>
+      <div v-for="user in this.$store.state.news" v-bind:key="user.id">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-import { fetchNewsList } from '../api/index.js'
+// import { fetchNewsList } from '../api/index.js'
 
 export default {
   data () {
@@ -15,18 +15,22 @@ export default {
     }
   },
   created() {
+    this.$store.dispatch('FETCH_NEWS');
     // 객체를 맵핑
-    var vm = this;
+    // var vm = this;
     // console.log('호출 전 :', this); // 전역 this
     // axios는 promise 기반
+    /**
     fetchNewsList()
       .then(response => {
         // 화살 함수 선언을 사용하면 vm = this 바인딩을 하지 않아도 this를 바로 사용할 수 있다.
         // then(function(response) { console.log(this);}) 이렇게 선언하면 this는 function 내부를 가르킨다.
         // console.log('호출 후 :', this);
-        vm.users = response.data;
+        // vm.users = response.data;
+        this.users = response.data;
       })
       .catch(err => console.error(err))
+     */
   }
 }
 </script>
