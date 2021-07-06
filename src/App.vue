d7e6b93e0cf80a3294c50c5977a0ba559c3de6b1
hk0305 <hyounkeun2009@gmail.com>
2021-07-06 18:54:12 +0900

    * 트랜지션을 사용한 Fade 기능 사용   - 부드러운 화면 전환 추가
    <transition name="page">
        <router-view></router-view>
    </transition>
    
    <style>
    /* Router Transiction */
    .page-enter-active, .page-leave-active {
      transition: opacity .5s;
    }
    .page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
      opacity: 0;
    }
    </style>

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,11 @@
     <!-- Window: Alt 누른 후 클릭 -->
     <!-- <ToolBal></ToolBal>와 동일하다 -->
     <tool-bar></tool-bar>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
     <!-- url 주소에 따른 view를 보여준다 -->
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
@@ -22,8 +25,16 @@ export default {
 </script>
 
 <style>
-  body {
-    padding: 0px;
-    margin: 0px;
-  }
+body {
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Router Transiction */
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to /* .page-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
